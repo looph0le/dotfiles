71f661ffd54ed832fd0dff8d38c2e6b32cbb23f9
looph0le <mitanshpanchal@gmail.com>
2021-12-21 04:02:00 +0530

    l

--- bar.sh
+++
@@ -1,4 +0,0 @@
-while true;
-do
-	xsetroot -name " [ $(date | cut -d' ' -f1,2,3,4 ) ] [ $(date | cut -d' ' -f5,6) ] [ $(pactl get-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo | sed -n 1p | cut -d"/" -f4)] [  $(free -m | sed -n 2p | cut -d" " -f21,22) / $(free -m | sed -n 2p | cut -d" " -f13) ]"
-done
