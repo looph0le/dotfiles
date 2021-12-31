bae503e3f6f3ee61c4b72250cab09d0867281112
looph0le <mitanshpanchal@gmail.com>
2021-12-31 14:26:10 +0530

    add kitty and bar config

--- .bar.sh
+++ .bar.sh
@@ -1,4 +1,4 @@
 while true;
 do
-	xsetroot -name " [ $(date | cut -d' ' -f1,2,3,4 ) ] [ $(date | cut -d' ' -f5,6) ] [ $(pactl get-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo | sed -n 1p | cut -d"/" -f4)] [  $(free -m | sed -n 2p | cut -d" " -f21,22) / $(free -m | sed -n 2p | cut -d" " -f13) ]"
+	xsetroot -name " [ $(date | cut -d' ' -f1,2,3,7) ] [ $(date | cut -d' ' -f4,5) ] [ $(pactl get-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo | sed -n 1p | cut -d"/" -f4)] [  "$(cat /sys/class/power_supply/BAT0/capacity)"] [  $(free -m | sed -n 2p | cut -d" " -f21,22)/$(free -m | sed -n 2p | cut -d" " -f13) ]"
 done
